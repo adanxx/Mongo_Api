44d1a8555ff2f181df04717d6fea07adddc7ce59
adanxx <abdi0361@stud.kea.dk>
2017-10-17 21:09:48 +0200

    MongoDB-Ap-Section.7-Lecture.66

---
+++ playground/mongo-connect-find.js
@@ -0,0 +1,76 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+//The statement is the same as previous, except this allow more flexbility as your are using 
+// a de-structuring instead of require-statment. with restructering it easier to use different
+// require-attribute without having to rewrite the samme command-line as line-1. The fallowing in
+
+const { MongoClient, ObjectID } = require('mongodb');
+
+/*/
+// line-7 gives you the access to 2 Mongo-library attribute. In f.eks. below: we can test ObejctID FROM the 
+// MongoDb library to print object-id like MongoDB-database for testing-consol.log or application-use.
+
+var obj = new ObjectID();
+console.log(obj);
+
+/*/
+
+// Connecting to the Monogdb with createde database TodoApps and testing the connection
+// with a callback function with the 2 fallowing statement -wheter the connection as succede or not?
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+
+    if (err) {
+        return Console.log('Unable to Connect to Server: Mongo')
+    }
+    console.log('Connect to MongoDB Server')
+    console.log();
+
+    //In this Section we trying to retrive or collection-data from the TodoApps-database   
+    //1.With the find-command is cusor/methods that point at the TodoApp 'Todos' Collection docs
+    // and let us different commands to manipulet data in specif directiv/Collection
+    //2. with toArray, we retrive the data as implied as Array of obejct with the set
+    // attributes and propeties.
+    //3.When the use a then-call to write the data to screen and callback-function in 
+    // case of error.
+
+    db.collection('Todos').find({ completed: false }).toArray().then((docs) => {
+
+        //.4 The find-command we show what type data we want return by passing in property
+        // in the parameter. By leaving the find-empty we retrive all the data. By f.eks
+        //below we testing by retriving object/collection with property:'completed set to false.
+
+
+        console.log('Todos: with false-statement');
+        console.log(JSON.stringify(docs, undefined, 2));
+
+    }, (err) => {
+        console.log('unable fetch the data', err);
+    });
+
+    //In the eksemple below we are retriving the wanted object/data by using the id with ObjectDB with required attribute
+    // MongoDB-lib 
+    db.collection('Todos').find({
+        _id: new ObjectID('59e648b3b9ffb0410e032f0d')    
+    }).toArray().then((docs) => {
+
+        console.log('Todos- with id: 59e648b3b9ffb0410e032f0d');
+        console.log(JSON.stringify(docs, undefined, 2));
+
+    }, (err) => {
+        console.log('unable fetch the data', err);
+    });
+
+ //In the eksemple below we are the count-function return the number of Collection retrived from todoApps-database. 
+    db.collection('Todos').find().count().then((count) => {
+        console.log('Todos Counts: '+count);
+    }, (err) => {
+        console.log('unable fetch the count', err);
+    });
+
+
+
+
+
+    // db.close();
+
+});
---
+++ playground/mongo-connect.js
@@ -0,0 +1,65 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+//The statement is the same as previous, except this allow more flexbility as your are using 
+// a de-structuring instead of require-statment. with restructering it easier to use different
+// require-attribute without having to rewrite the samme command-line as line-1. The fallowing in
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+/*/
+// line-7 gives you the access to 2 Mongo-library attribute. In f.eks. below: we can test ObejctID FROM the 
+// MongoDb library to print object-id like MongoDB-database for testing-consol.log or application-use.
+
+var obj = new ObjectID();
+console.log(obj);
+
+/*/
+
+// Connecting to the Monogdb with createde database TodoApps and testing the connection
+// with a callback function with the 2 fallowing statement -wheter the connection as succede or not?
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+
+    if (err) {
+        return Console.log('Unable to Connect to Server: Mongo')
+    }
+    console.log('Connect to MongoDB Server')
+    console.log();
+
+   /*/
+    // Adding  test data/row/Collection to the TodoApp database and testing with a callback funtion
+    // wheter the data was insert succefully or not into the TodoApp Todos row/Collection.
+    db.collection('Todos').insertOne({
+        text: 'Testing The TodoApp database',
+        completed: false
+
+    }, (err, result) => {
+
+        if (err) {
+            return console.log('Unable to insert TodoApp-database: Todos Collection', err)
+        }
+        //ops attribute store docs that where insert ->insertOne, insert one object/document
+        console.log(JSON.stringify(result.ops, undefined, 2))
+    });
+   /*/
+   /*/
+    db.collection('Users').insertOne({
+        Name: 'John Doe',
+        age: 25,
+        Location: 'New Metropolis'
+
+    }, (err, result2) => {
+        if (err) {
+            return console.log('Unable to insert TodoApp-database: User-Collectio', err)
+        }
+        //ops attribute allow you to pull docs that where insert ->insertOne, insert one object/document
+        // Think Arrays: under the console we retriving data from Users-Collection - property: fx - Id and Name
+        console.log( result2.ops[0]._id);
+        console.log( result2.ops[0].Name);
+        // Time-stamp function show time of doc/Collection was created
+        console.log( result2.ops[0]._id.getTimestamp());
+    });
+   /*/
+
+
+    db.close();
+});
