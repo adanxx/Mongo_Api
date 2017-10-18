7820dde140f9b16c36555c21258edadbdbc839e8
adanxx <abdi0361@stud.kea.dk>
2017-10-18 19:09:39 +0200

    Add validator and new Model

--- server/server.js
+++ server/server.js
@@ -10,28 +10,65 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 // what type data or property each fields/attributes can store. 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,  // with required: a string is not allowed to be empty
+        minlength: 1,    // String length > 1
+        trim: true       // trim removes withspace in the begin or end in string value
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false      // set it completed to false be default
     },
     completedApp: {
-        type: String
+        type: String,
+        default: null
     }
 });
 
-//3. we test our model by instance and insert data, and with simple save-statement..we have saved the data
-// into the Todo collection in our TodoApps-database 
+var Users = mongoose.model('User', {
+    /*/
+    name: {
+        type: String,
+        required: true,  // with required: a string is not allowed to be empty
+        minlength: 1,
+        trim: true 
+    },
+    /*/
+    email: {
+        type: String,
+        required: true,  // with required: a string is not allowed to be empty
+        minlength: 1,
+        trim: true       // trim removes withspace in the begin or end in string value
+    },
+});
+
+
+
+//3. we test our model by instance and insert data, and with simple save-statement..
+var user1 = new Users({
+  email: 'AdamJones@hotmail.com'
+});
+
 var newTodo = new Todo({
    text: 'This is sparta',
    completed: false
 });
+
+//4.we have saved the data into the Todo collection in our TodoApps-database 
+user1.save().then((docs)=>{
+   console.log('Saved the new User',docs );
+});
+
+
+/*/
 newTodo.save().then((doc)=>{
   console.log('Save to Todos-database', doc);
 }, (err)=>{
    console.log('Unable to Save ', err);
 });
 
+/*/
+/*/-------------------------------------Insert Second Data-type with new Model newTodo2
 var newTodo2 = new Todo({
     text: 'Space the final frontier',
     completed: true,
@@ -39,9 +76,13 @@ var newTodo2 = new Todo({
    
  });
 
-
  newTodo2.save().then((doc)=>{
    console.log(JSON.stringify(doc, undefined,2 ));
  }, (err)=>{
     console.log('Unable to Save ', err);
  });
+
+/*/
+
+
+
