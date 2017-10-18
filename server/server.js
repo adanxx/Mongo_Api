333e4ef5b13822510160d42205fe322257ab1f6f
adanxx <abdi0361@stud.kea.dk>
2017-10-18 23:19:51 +0200

    Add: GET-request

--- server/server.js
+++ server/server.js
@@ -38,12 +38,18 @@ app.post('/todos', (req, res)=>{
 
 });
 
+app.get('/todos', (req, res)=>{
+    Todo.find().then((todos) =>{
+        res.send({todos});
+    }, (err)=>{
+        res.status(400).send(err);
+    });
+});
+
 
 
 app.listen(3000, ()=>{
     console.log('Server Up on Port: 3000');
 })
-
-
 
 module.exports = {app};
